07261a2cd5733df20d28e0d8e716b64a01555862
kény Henry <henry.keny@outlook.fr>
2024-02-24 12:59:36 -0400

    closed #3

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('hello world')
+const express = require('express');
+
+const app = express();
+
+app.post('/webhook', express.json({type: 'application/json'}), (request, response) => {
+
+  response.status(202).send('Accepted');
+
+  const githubEvent = request.headers['x-github-event'];
+
+  if (githubEvent === 'issues') {
+    const data = request.body;
+    const action = data.action;
+    if (action === 'opened') {
+      console.log(`An issue was opened with this title: ${data.issue.title}`);
+    } else if (action === 'closed') {
+      console.log(`An issue was closed by ${data.issue.user.login}`);
+    } else {
+      console.log(`Unhandled action for the issue event: ${action}`);
+    }
+  } else if (githubEvent === 'ping') {
+    console.log('GitHub sent the ping event');
+  } else {
+    console.log(`Unhandled event: ${githubEvent}`);
+  }
+});
+
+const port = 3100;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
