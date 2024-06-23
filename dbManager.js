c3cb34173207b624c91d85d47182e9c0f23e3a9d
kény Henry <henry.keny@outlook.fr>
2024-06-23 13:53:58 -0400

    comment example

--- dbManager.js
+++ dbManager.js
@@ -25,38 +25,38 @@ class RedisClient {
   }
 }
 
-// Example usage
-const redisClient = new RedisClient({
-  host: 'localhost',
-  port: 63,
-});
+// // Example usage
+// const redisClient = new RedisClient({
+//   host: 'localhost',
+//   port: 6380,
+// });
 
-// Set a key-value pair
-redisClient.set('mykey', 'myvalue', (error, reply) => {
-  if (error) {
-    console.error('Error setting key:', error);
-  } else {
-    console.log('Key set:', reply);
-  }
-});
+// // Set a key-value pair
+// redisClient.set('mykey', 'myvalue', (error, reply) => {
+//   if (error) {
+//     console.error('Error setting key:', error);
+//   } else {
+//     console.log('Key set:', reply);
+//   }
+// });
 
-// Get the value for a key
-redisClient.get('mykey', (error, reply) => {
-  if (error) {
-    console.error('Error getting key:', error);
-  } else {
-    console.log('Value for key:', reply);
-  }
-});
+// // Get the value for a key
+// redisClient.get('mykey', (error, reply) => {
+//   if (error) {
+//     console.error('Error getting key:', error);
+//   } else {
+//     console.log('Value for key:', reply);
+//   }
+// });
 
-// Increment a counter
-redisClient.incr('counter', (error, reply) => {
-  if (error) {
-    console.error('Error incrementing counter:', error);
-  } else {
-    console.log('Counter:', reply);
-  }
-});
+// // Increment a counter
+// redisClient.incr('counter', (error, reply) => {
+//   if (error) {
+//     console.error('Error incrementing counter:', error);
+//   } else {
+//     console.log('Counter:', reply);
+//   }
+// });
 
-// Quit the client when done
-redisClient.quit();
+// // Quit the client when done
+// redisClient.quit();
